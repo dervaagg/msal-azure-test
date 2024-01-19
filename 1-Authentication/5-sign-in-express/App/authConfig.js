7b1ffb7814bc2df1ae66ef2bee9a6b68dabab3d5
dervaag <dervaag1122@gmail.com>
2024-01-19 08:19:21 +0700

    initial commit

--- 1-Authentication/5-sign-in-express/App/authConfig.js
+++ 1-Authentication/5-sign-in-express/App/authConfig.js
@@ -5,7 +5,7 @@
 
 require('dotenv').config();
 
-const TENANT_SUBDOMAIN = process.env.TENANT_SUBDOMAIN || 'Enter_the_Tenant_Subdomain_Here';
+const TENANT_SUBDOMAIN = process.env.TENANT_SUBDOMAIN || '16acd045-1e9a-462a-836a-71977f76daad';
 const REDIRECT_URI = process.env.REDIRECT_URI || 'http://localhost:3000/auth/redirect';
 const POST_LOGOUT_REDIRECT_URI = process.env.POST_LOGOUT_REDIRECT_URI || 'http://localhost:3000';
 
@@ -16,9 +16,9 @@ const POST_LOGOUT_REDIRECT_URI = process.env.POST_LOGOUT_REDIRECT_URI || 'http:/
  */
 const msalConfig = {
     auth: {
-        clientId: process.env.CLIENT_ID || 'Enter_the_Application_Id_Here', // 'Application (client) ID' of app registration in Azure portal - this value is a GUID
+        clientId: process.env.CLIENT_ID || '68583769-a0da-4093-8fed-e3890cf885b3', // 'Application (client) ID' of app registration in Azure portal - this value is a GUID
         authority: process.env.AUTHORITY || `https://${TENANT_SUBDOMAIN}.ciamlogin.com/`, // Replace the placeholder with your tenant name
-        clientSecret: process.env.CLIENT_SECRET || 'Enter_the_Client_Secret_Here', // Client secret generated from the app registration in Azure portal
+        clientSecret: process.env.CLIENT_SECRET || 'U418Q~pOYvKON4J2HRlfRCkEPF__QcWmvtqyXaUa', // Client secret generated from the app registration in Azure portal
     },
     system: {
         loggerOptions: {
